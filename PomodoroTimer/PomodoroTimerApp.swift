dda9824fbd8e02c52e6d6d896e7b0b2918a57e78
WatanabeMasaaki <86590348+WatanabeMasaaki@users.noreply.github.com>
2023-05-17 02:57:33 +0900

    開発再開

--- PomodoroTimer/PomodoroTimerApp.swift
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -6,16 +6,16 @@
 //
 
 import SwiftUI
-import FirebaseCore
+//import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-
-        return true
-    }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//
+//        return true
+//    }
+//}
 @main
 struct PomodoroTimerApp: App {
     var body: some Scene {
